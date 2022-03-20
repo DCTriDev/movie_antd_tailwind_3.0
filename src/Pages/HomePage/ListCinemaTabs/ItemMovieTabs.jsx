230b64d9e4cb80ea3031454b4a8b7c38f4b97a9c
DinhCongTri <dinhcongtri9597@gmail.com>
2022-03-20 12:53:52 +0700

    complete ItemMovieTabs

--- src/Pages/HomePage/ListCinemaTabs/ItemMovieTabs.jsx
+++ src/Pages/HomePage/ListCinemaTabs/ItemMovieTabs.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from "moment";
 
 
 function ItemMovieTabs({data}) {
@@ -9,15 +10,20 @@ function ItemMovieTabs({data}) {
                 data.danhSachPhim.map((movie, index) => {
                     return (
                         <div key={index} className='h-28 flex gap-3'>
-                            <div className='w-28'>
-                                <img className='h-28 w-28 object-cover' src={movie.hinhAnh} alt=""/>
-                            </div>
-                            <div className='flex flex-col'>
+                            <img className='w-28 h-full object-cover' src={movie.hinhAnh} alt="..."/>
+                            <div className='flex flex-col overflow-y-scroll flex-grow'>
                                 <h4>{movie.tenPhim}</h4>
-                                <div>
-                                    {movie.lstLichChieuTheoPhim.map((item,index) => {
+                                <div className='flex gap-3 flex-wrap'>
+                                    {movie.lstLichChieuTheoPhim.map((item, index) => {
                                         return (
-                                            <></>
+                                            <div
+                                                className='h-8 w-36 flex justify-center items-center bg-gray-200 border border-gray-300 rounded-lg cursor-pointer'
+                                                key={index}>
+                                                <span>
+                                                    {moment(item.ngayChieuGioChieu).format('DD/MM')}{' - '}
+                                                    {moment(item.ngayChieuGioChieu).format('hh:mm')}
+                                                </span>
+                                            </div>
                                         )
                                     })}
                                 </div>

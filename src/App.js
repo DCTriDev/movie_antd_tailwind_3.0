fdb0cd5d28252b6d22a9c6084914d74c29f9b1ae
DinhCongTri <dinhcongtri9597@gmail.com>
2022-03-24 13:12:54 +0700

    remove trash file

--- src/App.js
+++ src/App.js
@@ -2,30 +2,27 @@ import {BrowserRouter, Route, Switch} from "react-router-dom";
 import SignUpPage from "./Pages/SignUpPage/SignUpPage";
 import SignInPage from "./Pages/SignInPage/SignInPage";
 import HomePage from "./Pages/HomePage/HomePage";
-import DetailMovies from "./Pages/DetailMovies/DetailMovies";
 import Layout from "./Template/Layout";
-import LoadingAnimation from "./Components/LoadingAnimation/LoadingAnimation";
-import MovieBookingTicket from "./MovieTicket/MovieBookingTicket";
 
 function App() {
     return (
-       <div>
-           {/*<LoadingAnimation/>*/}
-           <BrowserRouter>
-               <Switch>
-                   <Route exact path='/' render={() => {
-                       return <Layout Component={HomePage}/>
+        <div>
+            {/*<LoadingAnimation/>*/}
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path='/' render={() => {
+                        return <Layout Component={HomePage}/>
 
-                   }}/>
-                   <Route exact path='/signup' component={SignUpPage}/>
-                   <Route exact path='/login' component={SignInPage}/>
-                   {/*<Route exact path='/movie/:id' render={({match}) => {*/}
-                   {/*    return <Layout maPhim={match.params.id} Component={DetailMovies}/>*/}
-                   {/*}}/>*/}
-               </Switch>
-           </BrowserRouter>
-           {/*<MovieBookingTicket/>*/}
-       </div>
+                    }}/>
+                    <Route exact path='/signup' component={SignUpPage}/>
+                    <Route exact path='/login' component={SignInPage}/>
+                    {/*<Route exact path='/movie/:id' render={({match}) => {*/}
+                    {/*    return <Layout maPhim={match.params.id} Component={DetailMovies}/>*/}
+                    {/*}}/>*/}
+                </Switch>
+            </BrowserRouter>
+            {/*<MovieBookingTicket/>*/}
+        </div>
     );
 }
 
--- src/MovieTicket/MapSeat.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function MapSeat() {
-    return (
-        <div></div>
-    );
-}
-
-export default MapSeat;
